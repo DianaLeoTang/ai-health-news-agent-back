1562a01a6e7368f42634ead7b9f90fcef49b210b
DianaLeoTang <wangyaotang0228@gmail.com>
2025-03-03 16:59:10 +0800

    feat:修改jest的配置

--- apps/front-end/jest.config.ts
+++ apps/front-end/jest.config.ts
@@ -1,6 +1,13 @@
+/*
+ * @Author: Diana Tang
+ * @Date: 2025-03-03 16:23:21
+ * @LastEditors: Diana Tang
+ * @Description: some description
+ * @FilePath: /AI-Health-News-Agent/apps/front-end/jest.config.ts
+ */
 export default {
   displayName: 'front-end',
-  preset: '../jest.preset.js',
+  preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
